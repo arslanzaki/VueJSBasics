a5d5909d63a6c3058102acaededb10fb96e130f0
arslanzaki <arslanzaki223@gmail.com>
2023-08-15 12:53:53 +0500

    Form Creation & Validation

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,21 @@
   <navbar :pages="pages" :active-page="activePage" :nav-link-click="(index) => (activePage = index)">
   </navbar>
 
-  <page-viewer v-if="pages.length > 0" :page="pages[activePage]"> </page-viewer>
+  <!-- <page-viewer v-if="pages.length > 0" :page="pages[activePage]"> </page-viewer> -->
+  <create-page :page-created="pageCreated">
+
+  </create-page>
 </template>
 
 <script>
 import PageViewer from "./components/PageViewer.vue";
 import Navbar from "./components/Navbar.vue";
+import CreatePage from "./components/CreatePage.vue"
 export default {
   components: {
     PageViewer,
     Navbar,
+    CreatePage
   },
   created() {
     this.getPages();
@@ -28,6 +33,10 @@ export default {
       let res = await fetch("pages.json");
       let data = await res.json();
       this.pages = data
+    },
+
+    pageCreated(pageObj) {
+      console.log(pageObj)
     }
   }
 };
---
+++ src/components/CreatePage.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="mb-3">
+        <form action="">
+            <div class="mb-3">
+                <label for="">Page Title</label>
+                <input type="text" v-model="pageTitle" />
+            </div>
+            <div class="mb-3">
+                <label for="">Content</label>
+                <textarea rows="5" v-model="pageContent"></textarea>
+            </div>
+            <div class="mb-3">
+                <button @click.prevent="submitForm">
+                    Create Page
+                </button>
+            </div>
+        </form>
+    </div>
+</template>
+
+
+<script>
+
+export default {
+    props: ["pageCreated"],
+
+    data() {
+        return {
+            pageTitle: "",
+            pageContent: ""
+        }
+    },
+
+    methods: {
+        submitForm() {
+            if (!this.pageTitle || !this.pageContent) {
+                alert("Please Fill The Form!");
+                return;
+            }
+
+            this.pageCreated({
+                pageTitle: this.pageTitle,
+                pageContent: this.pageContent
+            })
+        }
+    }
+}
+</script>
